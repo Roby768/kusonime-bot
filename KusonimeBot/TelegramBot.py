47c43b243bc87a3ba3ebb2ce7b50de8cc7695b1a
Roby768 <109590306+Roby768@users.noreply.github.com>
2022-07-29 02:18:39 +0700

    Update TelegramBot.py

--- KusonimeBot/TelegramBot.py
+++ KusonimeBot/TelegramBot.py
@@ -4,9 +4,9 @@ class TelegramBot(Client):
     def __init__(self, bot_token, **kwargs):
         super().__init__(
             session_name = "TelegramBot", 
-            api_id = 6, 
-            api_hash = "eb06d4abfb49dc3eeb1aeb98ae0f581e", 
-            bot_token = bot_token, 
+            api_id = 961780, 
+            api_hash = "bbbfa43f067e1e8e2fb41f334d32a6a7", 
+            bot_token = 1813711209:AAFrcQNty1tiKuTOvlfk9SZPwzMPuKTE4Nw, 
             plugins = dict(root = 'KusonimeBot/plugins'),
             **kwargs
         )
